1c0b687a0684ea2c8a1b80f4673431a3da82538d
Petr Altman <altick87@hotmail.com>
2018-08-07 09:54:18 +0200

    Expansions algorithm implemented with basic tests

--- src/t9-service.js
+++ src/t9-service.js
@@ -1,5 +1,37 @@
+const map = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: [ 'w', 'x', 'y', 'z'],
+}
+
 module.exports = {
-    getTextonyms(input) {
-        return 'hello ' + input;
+    getExpansions(input) {
+
+        let expansions = [];
+        Array.from(input).forEach((char, i) => {
+            let num = parseInt(char);
+            
+            let letters = map[num];
+            if(expansions.length == 0) {
+                // set initial expansions as the letters corresponding with the first number
+                expansions = [...letters];
+            } else {
+                // do expand with the current letter
+                let newExpansions = [];
+                expansions.forEach((expansion) => {
+                    letters.forEach((letter) => {
+                        newExpansions.push(expansion + letter);
+                    });
+                });
+                expansions = newExpansions;
+            }
+        });
+
+        return expansions;
     }
 }
--- src/t9-service.test.js
+++ src/t9-service.test.js
@@ -1,6 +1,36 @@
 const t9 = require('./t9-service');
 
-it('Should return correct textonyms', () => {
-    let result = t9.getTextonyms('world');
-    expect(result).toBe('hello world');
+it('Should return corresponding letters for the number when single number entered', () => {
+    let expected = ['a', 'b', 'c'];
+    let input = '2';
+    let result = t9.getExpansions(input);
+    expect(result).toEqual(expect.arrayContaining(expected));
+});
+
+it('Should return correct expansion for two numbers input', () => {
+    let expected = [
+        'ad', 'ae', 'af', 
+        'bd', 'be', 'bf', 
+        'cd', 'ce', 'cf'
+    ];
+    let input = '23';
+    let result = t9.getExpansions(input);
+    expect(result).toEqual(expect.arrayContaining(expected));
+});
+
+it('Should return correct expansion for three numbers input', () => {
+    let expected = [
+        'adg', 'adh', 'adi', 
+        'aeg', 'aeh', 'aei', 
+        'afg', 'afh', 'afi', 
+        'bdg', 'bdh', 'bdi', 
+        'beg', 'beh', 'bei', 
+        'bfg', 'bfh', 'bfi', 
+        'cdg', 'cdh', 'cdi', 
+        'ceg', 'ceh', 'cei', 
+        'cfg', 'cfh', 'cfi'
+    ];
+    let input = '234';
+    let result = t9.getExpansions(input);
+    expect(result).toEqual(expect.arrayContaining(expected));
 });
